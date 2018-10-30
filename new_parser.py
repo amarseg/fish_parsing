cf9d5ce7692cba8baa189714aa226f14847325fe
Unknown <liamarseg@gmail.com>
2018-10-30 21:15:51 +0000

    parser for the concatenated fish files

--- new_parser.py
+++ new_parser.py
@@ -4,23 +4,29 @@ import os
 import re
 import csv
 import pandas as pd
+import numpy as np
 
-folder = "test_data/"
+folder = "probes_data/"
 
 file_list = os.listdir(folder)
 file_list = [x for x in file_list if re.search('spots',x)]
-
+master_df = pd.DataFrame()
 for file in file_list:
     with open (folder + file,'r') as f:
         file_content = f.readlines()
     cell_names_index = [i for i, s in enumerate(file_content) if 'CELL' in s]
-    for i in cell_names_index:
+    cell_names_index.append(len(file_content))
+    column_name = file_content[cell_names_index[1]+4].split('\t')
+    for i in range(0,len(cell_names_index)-1):
         #Get all data for each dot in each cell, put in dataframe
-        data = file_content[cell_names_index[i]+5:cell_names_index[i+1]-1] 
+        data = file_content[cell_names_index[i]+5:cell_names_index[i+1]]
+        if(cell_names_index[i]+5 - cell_names_index[i+1]-1 < 0):
+            split_data = [row.split('\t') for row in data]
+            cell_name = file_content[cell_names_index[i]].split('\t')[1]
+            reshaped_data = np.array(split_data).reshape(-1,31)
+            df = pd.DataFrame(reshaped_data, columns =  column_name)
+            df['cell_name'] = cell_name.rstrip()
+            df['file_name'] = file
+            master_df = master_df.append(df, ignore_index = True)
 
-
-
-
-csvfile = open(folder + 'Results.txt','w')
-filewriter = csv.writer(csvfile, delimiter = '\t')
-filewriter.writerow(['Sample_Name']+['Cell'] +['Spots']+['Spots_data'])
+master_df.to_csv(folder + 'Results.csv')
---
+++ parser_one_file.py
@@ -0,0 +1,40 @@
+__author__ = 'am4613'
+
+import os
+import re
+import csv
+import pandas as pd
+import numpy as np
+
+file_name = "probes_data/new_data_spots.txt"
+
+with open (file_name,'r') as f:
+    file_content = f.readlines()
+
+file_index = [i for i, s in enumerate(file_content) if 'FISH-QUANT' in s]
+file_index.append(len(file_content))
+
+master_df = pd.DataFrame()
+for j in range(0, len(file_index)-1):
+    infile = file_content[file_index[j]:file_index[j+1]]
+
+    file_name_row = [i for i, s in enumerate(infile) if 'IMG_Raw' in s]
+    file_name_df = infile[file_name_row[0]].split('\t')[1]
+
+
+    cell_names_index = [i for i, s in enumerate(infile) if 'CELL' in s]
+    cell_names_index.append(len(infile))
+    column_name = infile[cell_names_index[1]+4].split('\t')
+    for i in range(0,len(cell_names_index)-1):
+        #Get all data for each dot in each cell, put in dataframe
+        data = infile[cell_names_index[i]+5:cell_names_index[i+1]]
+        if(cell_names_index[i]+5 - cell_names_index[i+1]-1 < 0):
+            split_data = [row.split('\t') for row in data]
+            cell_name = infile[cell_names_index[i]].split('\t')[1]
+            reshaped_data = np.array(split_data).reshape(-1,31)
+            df = pd.DataFrame(reshaped_data)
+            df['cell_name'] = cell_name.rstrip()
+            df['file_name'] = file_name_df
+            master_df = master_df.append(df, ignore_index = True)
+
+master_df.to_csv('probes_data/Results.csv')
